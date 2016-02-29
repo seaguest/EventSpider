911ce8c5b946fc5c227632fe1178d47f2ad57648
seaguest <tyhk1987@gmail.com>
2016-02-29 15:20:12 +0800

    integrated baidu map POI

--- eventSpider/util/locationUtil.py
+++ eventSpider/util/locationUtil.py
@@ -1,34 +1,99 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-
-from scrapy.item import Item, Field
-
-
-
 import urllib
 import json
 
-serviceurl = 'http://api.map.baidu.com/place/v2/search?'
+import difflib 
+import Levenshtein
 
-data = {'query':'华中科技大学', 'region':"武汉", 'output':'json', 'ak': 'sZWX5OEPWK0ezoPnFaKTfWs0'}
-# data = {'query':'华中科技大学', 'region':"武汉", 'ak': 'sZWX5OEPWK0ezoPnFaKTfWs0'}
-url = serviceurl + urllib.urlencode(data)
+from eventSpider.items.location import Location
+class LocationUtil():
 
-print 'Retrieving', url
+    @staticmethod
+    def computeSimilarity(text1, text2):
+        if not isinstance(text1, unicode):
+            text1 = unicode(text1, "utf-8") 
+        if not isinstance(text2, unicode):
+            text2 = unicode(text2, "utf-8") 
+        return difflib.SequenceMatcher(None, text1, text2).ratio()
 
-uh = urllib.urlopen(url)
-data = uh.read()
+    '''
+        a text match candidate, seems the same as above
+    '''
+    @staticmethod
+    def computeSimilarity2(text1, text2):
+        if not isinstance(text1, unicode):
+            text1 = unicode(text1, "utf-8") 
+        if not isinstance(text2, unicode):
+            text2 = unicode(text2, "utf-8") 
+        return Levenshtein.ratio(text1, text2)
+
+    @staticmethod
+    def getPOIs(region, address):
+        serviceurl = 'http://api.map.baidu.com/place/v2/search?'
+        data = {'region':region, 'query':address, 'output':'json', 'ak': 'sZWX5OEPWK0ezoPnFaKTfWs0'}
+        url = serviceurl + urllib.urlencode(data)
+        uh = urllib.urlopen(url)
+        data = uh.read()
+        try: js = json.loads(str(data))
+        except: js = None
+        
+        # print json.dumps(js, indent=4, ensure_ascii=False)
+
+        if 'status' not in js or js['status'] != 0:
+            print '==== Failure To Retrieve ===='
+        return js
+
+    @staticmethod
+    def getBestMatchedPOI(region, address):
+        # in case the similarity is less than 0.5, return None
+        maxSimilarity = 0.5
+        bestMatchedPOI = None
+        js = LocationUtil.getPOIs(region, add)
+        
+
+        for record in js['results']:
+            currentSimilarity = LocationUtil.computeSimilarity(record['name'], add),
+            if currentSimilarity > maxSimilarity:
+                maxSimilarity = currentSimilarity
+                bestMatchedPOI = record
+            
+        return bestMatchedPOI    
 
 
-try: js = json.loads(str(data))
+    @staticmethod
+    def createLocation(region, address):
+        location = Location()
+        location['region'] = region
+        matchedPOI = LocationUtil.getBestMatchedPOI(region, add)
+        if matchedPOI:
+            location['name'] = matchedPOI['name']
+            location['address'] = matchedPOI['address']
+        location['name'] = address
+
+        return location
 
 
 
-except: js = None
+region = "武汉"
+add = "华中科技大学"
+
+'''
+for record in js['results']:
+    print record['name'], add
+    print LocationUtil.computeSimilarity1(record['name'], add)
+'''
  
-if 'status' not in js or js['status'] != 'OK':
-    print '==== Failure To Retrieve ===='
- 
-print json.dumps(js, indent=4, ensure_ascii=False)
- 
+find = LocationUtil.createLocation(region, add)
+
+print repr(find)
+print type(find['name'])
+
+
+print unicode(find['name'], "utf-8")
+
+print find['name'].encode("utf-8")
+
+# print json.dumps(find, indent=4, ensure_ascii=False)
+
